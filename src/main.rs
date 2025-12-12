ee9a5d5db4de6d33c769f00be6c9bdfeb5399560
emily Crandall Fleischman <zyxw59@gmail.com>
2025-12-12 12:35:16 -0500

    wifi

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,29 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use defmt::info;
+use defmt::{info, warn};
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use esp_hal::clock::CpuClock;
-use esp_hal::i2c::master::I2c;
-use esp_hal::timer::timg::TimerGroup;
+use embassy_time::{Delay, Duration, Timer};
+use esp_hal::{
+    clock::CpuClock,
+    gpio::OutputPin,
+    i2c::master::{I2c, Instance as I2cInstance},
+    timer::timg::TimerGroup,
+};
+use esp_radio::wifi::{self, WifiController, WifiDevice, WifiEvent, WifiStaState};
 use {esp_backtrace as _, esp_println as _};
 
 extern crate alloc;
+
+macro_rules! make_static {
+    ($ty:ty, $val:expr $(,)?) => {{
+        static CELL: static_cell::StaticCell<$ty> = static_cell::StaticCell::new();
+        CELL.uninit().write($val)
+    }};
+}
+
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -34,31 +48,84 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
-    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
-    let (mut _wifi_controller, _interfaces) =
-        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
-            .expect("Failed to initialize Wi-Fi controller");
+    let radio_init = &*make_static!(
+        esp_radio::Controller,
+        esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller"),
+    );
+    let (wifi_controller, interfaces) = wifi::new(radio_init, peripherals.WIFI, Default::default())
+        .expect("Failed to initialize Wi-Fi controller");
+    let (_stack, runner) = embassy_net::new(
+        interfaces.sta,
+        embassy_net::Config::dhcpv4(Default::default()),
+        make_static!(
+            embassy_net::StackResources::<3>,
+            embassy_net::StackResources::<3>::new(),
+        ),
+        0,
+    );
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    spawner.spawn(wifi_connection(wifi_controller)).unwrap();
+    spawner.spawn(net_runner(runner)).unwrap();
 
-    let i2c = I2c::new(peripherals.I2C0, Default::default())
-        .unwrap()
-        .with_scl(peripherals.GPIO32)
-        .with_sda(peripherals.GPIO33);
-
-    let mut delay = esp_hal::delay::Delay::new();
-    let mut bme = bme280::i2c::BME280::new_primary(i2c);
-    bme.init(&mut delay).unwrap();
+    let mut bme = initialize_bme(peripherals.I2C0, peripherals.GPIO32, peripherals.GPIO33);
 
     loop {
-        let measurements = bme.measure(&mut delay).unwrap();
+        let measurements = bme.measure(&mut Delay).unwrap();
         info!(
-            "temperature: {} °C, pressure: {} Pa, humidity: {}%",
-            measurements.temperature, measurements.pressure, measurements.humidity
+            "temperature: {=f32} °C, pressure: {=f32} Pa, humidity: {=f32}%",
+            measurements.temperature, measurements.pressure, measurements.humidity,
         );
         Timer::after(Duration::from_secs(60)).await;
     }
+}
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
+#[embassy_executor::task]
+async fn wifi_connection(mut controller: WifiController<'static>) {
+    info!("starting wifi connection task");
+    loop {
+        if wifi::sta_state() == WifiStaState::Connected {
+            controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            warn!("wifi disconnected");
+            Timer::after(Duration::from_secs(5)).await;
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            // configure and start wifi
+            let config = wifi::ModeConfig::Client(
+                wifi::ClientConfig::default()
+                    .with_ssid(SSID.into())
+                    .with_password(PASSWORD.into()),
+            );
+            controller.set_config(&config).unwrap();
+            info!("starting wifi");
+            controller.start_async().await.unwrap();
+            info!("wifi started");
+        }
+        info!("wifi connecting");
+        match controller.connect_async().await {
+            Ok(()) => info!("wifi connected"),
+            Err(e) => {
+                warn!("failed to connect to wifi: {:?}", e);
+                Timer::after(Duration::from_secs(5)).await;
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn net_runner(mut runner: embassy_net::Runner<'static, WifiDevice<'static>>) {
+    runner.run().await
+}
+
+fn initialize_bme(
+    i2c: impl I2cInstance + 'static,
+    scl: impl OutputPin + 'static,
+    sda: impl OutputPin + 'static,
+) -> bme280::i2c::BME280<I2c<'static, esp_hal::Blocking>> {
+    let i2c = I2c::new(i2c, Default::default())
+        .unwrap()
+        .with_scl(scl)
+        .with_sda(sda);
+    let mut bme = bme280::i2c::BME280::new_primary(i2c);
+    bme.init(&mut Delay).unwrap();
+    bme
 }
